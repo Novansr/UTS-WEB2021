12c9b6fa59a93917f69259efabf984d21df41f3d
Muhammad Novan Saputra <93202567+Novansr@users.noreply.github.com>
2022-12-20 07:35:30 +0800

    Delete UTS_WEB-main directory

--- UTS_WEB-main/logic.js
+++
@@ -1,9 +0,0 @@
-alert("login sebagai user\nusername : user\npassword : uts");
-alert("login sebagai admin\nusername : admin\npassword : uts");
-
-document.getElementById("btn").addEventListener("click", function(){
-    document.querySelector(".pop").style.display = "flex";
-    document.querySelector(".isi2").style.display = "flex";
-    document.querySelector(".container").style.display = "none"
-})
-
